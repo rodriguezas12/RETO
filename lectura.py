cd85c718bf09ece5610858a8800f1a57490de912
SergioXodriguez <110206978+SergioXodriguez@users.noreply.github.com>
2024-03-28 20:17:35 -0500

    portales (100% terminado), pick in (70% terminado), estaciones (50% terminado)

--- lectura.py
+++ lectura.py
@@ -1,20 +1,48 @@
 from reader import R420
 import sllurp
 import numpy as np
-reader = R420('192.168.0.20')
+import pandas as pd
+import os
+import keyboard
+from datetime import datetime
+# Configuración de conexión a MySQL
+import mysql.connector
+from mysql.connector import errorcode
 
-freqs = reader.freq_table
-powers = reader.power_table
+
+reader_IN = R420('192.168.0.20')
+reader_OUT = R420('192.168.0.21')
+
+freqs = reader_IN.freq_table
+powers = reader_OUT.power_table
+
+
+
 while True:
-    tags = reader.detectTags(powerDBm=powers[-1], freqMHz=freqs[0], 
+    tags_IN = reader_IN.detectTags(powerDBm=powers[-1], freqMHz=freqs[0], 
+        mode=1002, session=2, population=1, duration=0.5, searchmode=2)
+    tags_OUT = reader_OUT.detectTags(powerDBm=powers[-1], freqMHz=freqs[0], 
         mode=1002, session=2, population=1, duration=0.5, searchmode=2)
 
-    
-    for tag in tags:
+    for tag in tags_IN:
         first_value = tag['EPC-96'][0:24].decode('utf-8')
-        print(first_value)
-        #print(tag)
+      
+        #print(first_value)
+
+ 
+    for tag in tags_IN:
+        first_value = tag['EPC-96'][0:24].decode('utf-8')
+      
+        #print(first_value)
+
+
+
+
+
+
+
+
+
+
 
         
-
-
