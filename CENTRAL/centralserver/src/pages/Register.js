11ecd75018516ac51c607f0e78b62dbc82d414df
Mighty-Fight <amantillac@uninorte.edu.co>
2024-04-21 01:32:00 -0500

    modified:   CENTRAL/centralserver/src/pages/Register.js

--- CENTRAL/centralserver/src/pages/Register.js
+++ CENTRAL/centralserver/src/pages/Register.js
@@ -1,26 +1,12 @@
 import Header from "../components/Header";
-import './Register.css'; // Importamos el archivo CSS para aplicar estilos
 
 export default function Register() {
   return (
-    <div className="container"> {/* Agregamos una clase para el contenedor */}
+    <>
       <Header />
       <h2>Pagina de registro</h2>
-      <div>
-        <label htmlFor="username">Usuario:</label>
-        <input type="text" id="username" name="username" />
-      </div>
-      <div>
-        <label htmlFor="password">Contraseña:</label>
-        <input type="password" id="password" name="password" />
-      </div>
-      <div>
-        <button onClick={handleClick}>Comprobar</button>
-      </div>
-    </div>
+      <h3>Usuario</h3>
+      <h4>Contraseña</h4>
+    </>
   );
-}
-
-function handleClick() {
-  alert('Mensaje de alerta negativo');
 }
