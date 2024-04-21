b1464fa58d20c3fda7ee02c5b9fed121bf7eb5df
Arias3 <juan43arias@gmial.com>
2024-04-21 00:31:38 -0500

    modified:   CENTRAL/centralserver/src/App.js 	modified:   CENTRAL/centralserver/src/index.js

--- CENTRAL/centralserver/src/index.js
+++ CENTRAL/centralserver/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,7 +10,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Si deseas comenzar a medir el rendimiento en tu aplicación, pasa una función
+// para registrar los resultados (por ejemplo: reportWebVitals(console.log))
+// o envíalos a un punto de análisis. Aprende más: https://bit.ly/CRA-vitals
 reportWebVitals();
