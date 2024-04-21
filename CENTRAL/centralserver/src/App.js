4bcfd55be03a3a21154051529051c757b188fbc4
Mighty-Fight <amantillac@uninorte.edu.co>
2024-04-21 01:30:25 -0500

    modified:   CENTRAL/centralserver/src/App.js

--- CENTRAL/centralserver/src/App.js
+++ CENTRAL/centralserver/src/App.js
@@ -1,30 +1,8 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
 import { BrowserRouter, Route, Routes } from "react-router-dom";
->>>>>>> 77b8f58b319a4df59cb9f2c5299cc830a763de5b
 import Home from "../src/pages/Home";
 import Login from "../src/pages/Login";
 import Nopage from "../src/pages/Nopage";
 import Register from "../src/pages/Register";
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Nopage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-=======
-
-
 
 function App() {
   return (
@@ -36,8 +14,8 @@ function App() {
         <Route path="/*" element={<Nopage />}/>
       </Routes>
     </BrowserRouter>
->>>>>>> 77b8f58b319a4df59cb9f2c5299cc830a763de5b
   );
 }
 
 export default App;
+
