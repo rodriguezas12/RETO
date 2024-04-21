933a36febfb5f37171c1ee86dacb4aeab8f38e6d
Mighty-Fight <amantillac@uninorte.edu.co>
2024-04-21 01:24:01 -0500

    modified:   CENTRAL/centralserver/package-lock.json 	modified:   CENTRAL/centralserver/package.json 	modified:   CENTRAL/centralserver/src/App.js 	modified:   CENTRAL/centralserver/src/pages/Register.js

--- CENTRAL/centralserver/src/pages/Register.js
+++ CENTRAL/centralserver/src/pages/Register.js
@@ -4,7 +4,16 @@ export default function Register() {
   return (
     <>
       <Header />
-      <h2>Registter Page</h2>
+      <h2>Pagina de registro</h2>
+      <div>
+        <label htmlFor="username">Usuario:</label>
+        <input type="text" id="username" name="username" />
+      </div>
+      <div>
+        <label htmlFor="password">Contraseña:</label>
+        <input type="password" id="password" name="password" />
+      </div>
     </>
   );
 }
+
