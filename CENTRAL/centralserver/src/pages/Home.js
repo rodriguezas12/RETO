0e0076507793cae5c1dce9c874d9a40fb12f7de0
Mighty-Fight <amantillac@uninorte.edu.co>
2024-04-24 00:51:51 -0500

    modified:   CENTRAL/centralserver/src/pages/Home.css 	modified:   CENTRAL/centralserver/src/pages/Home.js

--- CENTRAL/centralserver/src/pages/Home.js
+++ CENTRAL/centralserver/src/pages/Home.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import './Home.css';
+import logo from '../Media/logo.png'
+
 export default function Home() {
   return (
     <>
       <h2>Home Page</h2>
+      <div className="login-box">
+        <img src={logo}alt="Logo" />
+        <h3>PORTAL DE GESTION INDUSTRIAL</h3>
+        <form>
+          <label>INGRESE SU USUARIO:</label>
+          <input type="text" placeholder="Usuario" />
+          <button type="submit">Iniciar Sesión</button>
+        </form>
+        <button>Registrarse</button>
+      </div>
     </>
   );
 }
