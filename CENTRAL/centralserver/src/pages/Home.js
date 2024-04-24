75b5525ec1f80237712bb104fcd65feb739b7b28
Mighty-Fight <amantillac@uninorte.edu.co>
2024-04-24 01:09:15 -0500

    modified:   CENTRAL/centralserver/src/pages/Home.css modified:   CENTRAL/centralserver/src/pages/Home.js modified:   CENTRAL/centralserver/src/pages/Register.css modified:   CENTRAL/centralserver/src/pages/Register.js

--- CENTRAL/centralserver/src/pages/Home.js
+++ CENTRAL/centralserver/src/pages/Home.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './Home.css';
-import logo from '../Media/logo.png'
+import logo from '../Media/logo.png';
+import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
 
 export default function Home() {
   return (
     <>
       <h2>Home Page</h2>
       <div className="login-box">
-        <img src={logo}alt="Logo" />
+        <img src={logo} alt="Logo" />
         <h3>PORTAL DE GESTION INDUSTRIAL</h3>
         <form>
           <label>INGRESE SU USUARIO:</label>
           <input type="text" placeholder="Usuario" />
-          <button type="submit">Iniciar Sesión</button>
+          {/* Enlace al componente Login */}
+          <Link to="/login">
+            <button type="button">Iniciar Sesión</button>
+          </Link>
         </form>
-        <button>Registrarse</button>
+        {/* Enlace al componente Register */}
+        <Link to="/register">
+          <button type="button">Registrarse</button>
+        </Link>
       </div>
     </>
   );
--- CENTRAL/centralserver/src/pages/Register.js
+++ CENTRAL/centralserver/src/pages/Register.js
@@ -1,7 +1,23 @@
+import React from 'react';
+import './Register.css';
+import logo from '../Media/logo.png';
+
 export default function Register() {
   return (
     <>
-      <h2>hola login</h2>
+      <h2>REGISTRATE</h2>
+      <div className="login-box">
+        <img src={logo} alt="Logo" />
+        <form>
+          <label>NOMBRE COMPLETO:</label>
+          <input type="text" placeholder="Nombre completo" />
+          <label>CODIGO ESTUDIANTIL:</label>
+          <input type="text" placeholder="Código estudiantil" />
+          <label>NRC:</label>
+          <input type="text" placeholder="NRC" />
+          <button type="submit">Registrar</button>
+        </form>
+      </div>
     </>
   );
 }
