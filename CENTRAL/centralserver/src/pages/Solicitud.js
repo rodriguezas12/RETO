33f4a88d712993665ba5cbb398b105a304684c55
Arias3 <juan43arias@gmial.com>
2024-04-25 14:06:48 -0500

    modified:   CENTRAL/centralserver/src/pages/Solicitud.js

--- CENTRAL/centralserver/src/pages/Solicitud.js
+++ CENTRAL/centralserver/src/pages/Solicitud.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import './Header.css'; // Importa los estilos CSS para el encabezado
-const Header = ({ imageUrl, text }) => {
-  return (
-    <header className="header">
-      <img src={imageUrl} alt="Logo" className="header-image" />
-      <h1 className="header-text">{text}</h1>
-    </header>
+import React from "react";
 
 
-
-
-  );
+export default function Solicitud() {
+    return (
+      <>
+        <h1>davo</h1>
+        <h2>Bienvenido mi papá</h2> {/* Usar el componente PaginaLogged */}
+      </>
+    );
 }
-
-export default Header;
