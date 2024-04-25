9d13f90d29d58ce523a61a913aa0537910ca1711
Mighty-Fight <amantillac@uninorte.edu.co>
2024-04-25 08:38:26 -0500

    modified:   CENTRAL/centralserver/NOTAS DESARROLLADORES modified:   CENTRAL/centralserver/src/pages/Login.js

--- CENTRAL/centralserver/src/pages/Login.js
+++ CENTRAL/centralserver/src/pages/Login.js
@@ -5,7 +5,7 @@ export default function Login() {
     return (
       <>
         <h1>davo</h1>
-        <h2>Bienvenido mi papá</h2> {/* Usar el componente PaginaLogged */}
+        <h2>Bienvenido, estas correctamente loogeado</h2> {/* Usar el componente PaginaLogged */}
       </>
     );
 }
