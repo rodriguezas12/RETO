9cbfa97757ebd926286674e1006f5ce51c426985
Mighty-Fight <amantillac@uninorte.edu.co>
2024-04-25 09:44:45 -0500

    new file:   CENTRAL/centralserver/backend/servidor.js

---
+++ CENTRAL/centralserver/backend/servidor.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: 'db-retorfid.cdsc040qszy0.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'usuario123',
+  database: 'estaciones'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a la base de datos:', err);
+    process.exit(1);
+  }
+  console.log('Conexión a la base de datos establecida');
+  console.log('El servidor de base de datos está corriendo en el puerto:', db.config.port);
+});
+
+
+const PORT = 5000; // Cambio del puerto a 5000
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
