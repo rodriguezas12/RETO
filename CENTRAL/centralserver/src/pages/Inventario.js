ff00949c724d6d6fea0d9ca4b43a39d97da45013
michelle <michellejs@uninorte.edu.co>
2024-04-27 15:59:56 -0400

    modified:   CENTRAL/centralserver/src/pages/Inventario.js

--- CENTRAL/centralserver/src/pages/Inventario.js
+++ CENTRAL/centralserver/src/pages/Inventario.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { tag: 'Tag1', kit: 'Kit1', bahia: 'Bahía1', tiempoIngreso: '09:00', tiempoAlmacen: '2 horas' },
+        { tag: 'Tag2', kit: 'Kit2', bahia: 'Bahía2', tiempoIngreso: '10:00', tiempoAlmacen: '1 hora' },
+        // Agrega más objetos de datos según sea necesario
+      ]
+    };
+  }
 
-export default function Inventario() {
+  render() {
     return (
-      <>
-        <h1>davo</h1>
-        <h2>Bienvenido mi papá</h2> {/* Usar el componente PaginaLogged */}
-      </>
+      <div>
+        <h1>Tabla de inventario</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Tag</th>
+              <th>Kit</th>
+              <th>Bahía</th>
+              <th>Tiempo de ingreso</th>
+              <th>Tiempo en el almacén</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.tag}</td>
+                <td>{item.kit}</td>
+                <td>{item.bahia}</td>
+                <td>{item.tiempoIngreso}</td>
+                <td>{item.tiempoAlmacen}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
+  }
 }
