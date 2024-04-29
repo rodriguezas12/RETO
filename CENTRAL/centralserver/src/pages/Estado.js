888ec2a2cdc536d941ca92e46d85bb99e2570b71
Aronnam117 <63078526+Aronna117@users.noreply.github.com>
2024-04-29 18:11:29 -0500

    añadir botones de regreso al menu en todos los servicios

--- CENTRAL/centralserver/src/pages/Estado.js
+++ CENTRAL/centralserver/src/pages/Estado.js
@@ -4,6 +4,8 @@ import React from "react";
 export default function Estado() {
     return (
       <>
+        <button className="back-button" onClick={() =>
+        window.location.href = '/Menu'}>Regresar al Menú</button>
         <h1>davo</h1>
         <h2>Bienvenido, estas correctamente loogeado</h2> {/* Usar el componente PaginaLogged */}
       </>
