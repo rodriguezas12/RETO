0d378e65a417c45558ac964530d276076b3f0554
Mighty-Fight <amantillac@uninorte.edu.co>
2024-05-06 20:04:01 -0500

    modified:   CENTRAL/centralserver/src/App.js new file:   CENTRAL/centralserver/src/components/PrivateRoute.js 	modified:   CENTRAL/centralserver/src/pages/Home.js

--- CENTRAL/centralserver/src/App.js
+++ CENTRAL/centralserver/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// App.js
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Estado from "./pages/Estado";
 import Home from "./pages/Home";
 import IngresoMaterial from "./pages/IngresoMaterial";
@@ -10,16 +11,24 @@ import Register from "./pages/Register";
 import Solicitud from "./pages/Solicitud";
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home setAuthenticated={setAuthenticated} />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/solicitud" element={<Solicitud />} />
-        <Route path="/inventario" element={<Inventario />} />
-        <Route path="/ingresomaterial" element={<IngresoMaterial />} />
-        <Route path="/estado" element={<Estado />} />
+        {authenticated ? (
+          <>
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/solicitud" element={<Solicitud />} />
+            <Route path="/inventario" element={<Inventario />} />
+            <Route path="/ingresomaterial" element={<IngresoMaterial />} />
+            <Route path="/estado" element={<Estado />} />
+          </>
+        ) : (
+          <Navigate to="/" replace />
+        )}
         <Route path="*" element={<Nopage />} />
       </Routes>
     </BrowserRouter>
