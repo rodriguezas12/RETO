bc542d991988ef90a1fd2320791da151858730ad
michelle <michellejs@uninorte.edu.co>
2024-05-23 11:46:47 -0400

    modified:   CENTRAL/centralserver/src/components/header.jsx 	modified:   CENTRAL/centralserver/src/pages/Estado.css 	modified:   CENTRAL/centralserver/src/pages/Inventario.css

--- CENTRAL/centralserver/src/components/header.jsx
+++ CENTRAL/centralserver/src/components/header.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import logo from "../Media/logo.png"; // Importa la imagen
 
 const Header = ({ titulo }) => {
-  const styles = {
+  const [styles, setStyles] = useState({
     headerContainer: {
       position: "relative",
-      width: "90vw", // Cambiado a 100% para ocupar todo el ancho
+      width: "90vw",
       display: "flex",
       alignItems: "center",
-      justifyContent: "space-between", // Cambiado a "space-between" para distribuir los elementos
+      justifyContent: "space-between",
       padding: "10px",
       margin: "10px",
       backgroundColor: "#FEFAF6",
@@ -20,11 +20,11 @@ const Header = ({ titulo }) => {
       flex: "1",
       textAlign: "center",
       margin: "0",
-      fontSize: "4vh", // Tamaño relativo al alto de la ventana
+      fontSize: "4vh",
       color: "#8b4513",
       whiteSpace: "nowrap",
       fontFamily: "ChauPhilomeneOne-Regular",
-      maxWidth: "60%", // Establece un ancho máximo para el texto
+      maxWidth: "60%",
     },
     backButton: {
       fontFamily: "Nunito",
@@ -33,27 +33,58 @@ const Header = ({ titulo }) => {
       color: "white",
       border: "none",
       borderRadius: "20px",
-      boxShadow: "0 6px 6px 0 rgba(0, 0, 0, 0.5)", // Sombras más pronunciadas en estado de hover
+      boxShadow: "0 6px 6px 0 rgba(0, 0, 0, 0.5)",
       fontWeight: "bold",
       cursor: "pointer",
-      transition: "background-color 0.3s, box-shadow 0.3s", // Transiciones suaves para el color de fondo y la sombra
-      fontSize: "2.5vh", // Tamaño relativo al alto de la ventana
+      transition: "background-color 0.3s, box-shadow 0.3s",
+      fontSize: "2.5vh",
       maxWidth: "200px",
       marginBottom: "10px",
-      // Efecto hover
       "&:hover": {
-        backgroundColor: "#000000", // Cambia el color de fondo al pasar el cursor
-        boxShadow: "0 6px 12px 0 rgba(0, 0, 0, 0.5)", // Sombras más pronunciadas en estado de hover
+        backgroundColor: "#000000",
+        boxShadow: "0 6px 12px 0 rgba(0, 0, 0, 0.5)",
       },
     },
     logo: {
-      width: "auto", // Cambiado a "auto" para mantener la proporción de la imagen
-      height: "80px", // Ajustado a una altura específica
+      width: "auto",
+      height: "80px",
     },
+  });
+
+  const handleResize = () => {
+    const newStyles = {
+      ...styles,
+      headerText: {
+        ...styles.headerText,
+        fontSize: window.innerWidth <= 600 ? "2.5vh" :
+          window.innerWidth <= 768 ? "3vh" :
+            window.innerWidth <= 1024 ? "3.5vh" : "4vh",
+      },
+      backButton: {
+        ...styles.backButton,
+        fontSize: window.innerWidth <= 600 ? "2vh" :
+          window.innerWidth <= 768 ? "2.3vh" :
+            window.innerWidth <= 1024 ? "2.5vh" : "2.5vh",
+      },
+      logo: {
+        ...styles.logo,
+        height: window.innerWidth <= 600 ? "50px" :
+          window.innerWidth <= 768 ? "60px" :
+            window.innerWidth <= 1024 ? "70px" : "80px",
+      }
+    };
+
+    setStyles(newStyles);
   };
 
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const handleBackButtonClick = () => {
-    window.history.back(); // Utiliza window.history.back() para retroceder en el historial del navegador
+    window.history.back();
   };
 
   return (
